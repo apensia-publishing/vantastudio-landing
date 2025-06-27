c664dc55700a25882cc9a8f6ce36057d7f4a7599
donghhan <apensia914@gmail.com>
2025-06-27 22:18:10 +0700

    HeroSection added with HamburgerButton open animation

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,12 +1,81 @@
+import { useState, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
+type HamburgerButtonProps = {
+  hamburgerOpen: boolean;
+};
+
+function HamburgerButton({
+  hamburgerOpen,
+  ...props
+}: HamburgerButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  const hamburgerButtonRef = useRef(null);
+  useGSAP(
+    () => {
+      if (hamburgerOpen) {
+        gsap.to("#top_line", {
+          scaleX: 0,
+          duration: 0.25,
+          transformOrigin: "right",
+        });
+        gsap.to("#middle_line", {
+          scaleX: 0,
+          duration: 0.25,
+          transformOrigin: "right",
+        });
+        gsap.to("#bottom_line", {
+          scaleX: 0,
+          duration: 0.25,
+          transformOrigin: "right",
+        });
+      }
+    },
+    { scope: hamburgerButtonRef, dependencies: [hamburgerOpen] }
+  );
+
+  return (
+    <button
+      id="hamburger_button"
+      className="w-8 h-5 flex flex-col items-end justify-center relative"
+      ref={hamburgerButtonRef}
+      {...props}
+    >
+      <div
+        aria-label="Top line"
+        id="top_line"
+        className="w-3/4 h-0.5 bg-white absolute top-0 right-0"
+      />
+      <div
+        aria-label="Middle line"
+        className="w-1/2 h-0.5 bg-white"
+        id="middle_line"
+      />
+      <div
+        aria-label="Bottom line"
+        className="w-full h-0.5 bg-white absolute bottom-0 right-0"
+        id="bottom_line"
+      />
+    </button>
+  );
+}
+
 export default function HeroSection() {
+  const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+  console.log(hamburgerOpen);
+  const handleHamburgerOpen = () => setHamburgerOpen(true);
+
   return (
     <section
       id="hero"
       className="w-full h-svh bg-[url('../hero_bg.jpg')] bg-cover bg-no-repeat p-4 text-white flex flex-col justify-between"
-      // style={{ border: "1px solid red" }}
     >
-      <div>
+      <div className="w-full flex justify-between items-center">
         <h1 className="uppercase text-[2rem]">vanda studio</h1>
+        <HamburgerButton
+          hamburgerOpen={hamburgerOpen}
+          onClick={handleHamburgerOpen}
+        />
       </div>
       <div>
         <p className="leading-6 text-md">
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ReactLenis } from "lenis/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ReactLenis>
+      <App />
+    </ReactLenis>
   </StrictMode>
 );

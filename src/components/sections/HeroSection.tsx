96443ad39a283a1598d7a787db80f7a657197e6d
donghhan <apensia914@gmail.com>
2025-07-02 06:12:59 +0700

    HeroSection footer responsive

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -195,6 +195,17 @@ export default function HeroSection() {
           Show More
         </Link>
       </section>
+
+      {/* Footer section */}
+      <footer id="footer" className="w-full bg-black p-4 sm:p-6">
+        <h1 className="text-white uppercase text-[13.5vw] lg:text-[13.8vw] text-nowrap pt-5">
+          vanda studio
+        </h1>
+        <div className="text-white flex justify-between mt-5 lg:mt-0">
+          <span className="text-md lg:text-lg">2025 vanda studio</span>
+          <span className="text-md lg:text-lg">All rights reserved.</span>
+        </div>
+      </footer>
     </>
   );
 }
